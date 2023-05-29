fb0d77efc5339d74d8d050ad738218a37284ab80
Houssam-Eddine Mney <houssam.mney@gmail.com>
2023-05-29 20:42:25 +0100

    init project

---
+++ XKCD/XKCDApp.swift
@@ -0,0 +1,17 @@
+//
+//  XKCDApp.swift
+//  XKCD
+//
+//  Created by Houssam-Eddine Mney on 29/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct XKCDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
