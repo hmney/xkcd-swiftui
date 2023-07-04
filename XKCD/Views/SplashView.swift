293792c3fcd5618dc086a183c91bd1b5388695c8
Houssam-Eddine Mney <houssam-eddine.mney@ibm.com>
2023-07-04 23:30:31 +0100

    Add splash view

---
+++ XKCD/Views/SplashView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  XKCD
+//
+//  Created by Houssam-Eddine Mney on 29/5/2023.
+//
+
+import SwiftUI
+import Combine
+
+struct SplashView: View {
+    @State private var isActive = false
+   
+    var body: some View {
+
+        VStack {
+            Image("splash")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding([.leading, .trailing], 20)
+            
+            ProgressView()
+            Text("Data Loading ...")
+                .padding()
+            
+            
+        }
+        
+    }
+    
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
