5032ee7012464c10e4e563600023b695d61de80e
Houssam-Eddine Mney <houssam-eddine.mney@ibm.com>
2023-07-04 23:22:14 +0100

    Create comic model

---
+++ XKCD/Models/Comic.swift
@@ -0,0 +1,40 @@
+//
+//  ComicsModel.swift
+//  XKCD
+//
+//  Created by Houssam-Eddine Mney on 30/5/2023.
+//
+
+import Foundation
+
+class Comic: Codable, Hashable {
+    static func == (lhs: Comic, rhs: Comic) -> Bool {
+        lhs.num == rhs.num
+        
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(num)
+    }
+    
+    var title: String?
+    var safeTitle: String?
+    var alt: String?
+    var image: String?
+    var num: Int?
+    var link: String?
+    var day: String?
+    var month: String?
+    var year: String?
+    var news: String?
+    var transcript: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case title, alt, num, link, day, month, year, news, transcript
+        
+        case safeTitle = "safe_title"
+        case image = "img"
+    }
+    
+    
+}
