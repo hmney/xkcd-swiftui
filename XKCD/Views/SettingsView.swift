05a916c7294123f9068f857f3cac1aff9b9a5e47
Houssam-Eddine Mney <houssamemney98@gmail.com>
2023-07-06 13:28:36 +0100

    Add a setting view

---
+++ XKCD/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  XKCD
+//
+//  Created by Houssam-Eddine Mney on 30/5/2023.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("SettingsView")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
